69dd5cf495bb2368f1ca07cf83b23c69ee03766a
Avyan Mehra <avyan_mehra@s2023.ssts.edu.sg>
2025-11-07 07:20:41 +0800

    Initial Commit

---
+++ VisionDemo2/VisionDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  VisionDemo2App.swift
+//  VisionDemo2
+//
+//  Created by Avyan Mehra on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct VisionDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
